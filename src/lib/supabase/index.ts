b8f8b02589bd161ba1be7560c61f909bf7c923a6
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-10-02 23:57:39 -0400

    remove console log

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,14 +1,8 @@
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export let supabaseServerClient: ReturnType<typeof createSupabaseServerClient>;
-
-console.log('backend supabase:', {
-  GITHUB_ID,
-  GITHUB_SECRET,
-});
 
 export const initSupabaseServerClient = (event: RequestEvent) => {
   supabaseServerClient = createSupabaseServerClient({
