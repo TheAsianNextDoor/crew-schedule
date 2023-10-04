29e4a657a4fbb4868ef4a9a1abacbe2d14f1ee69
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-10-03 22:35:14 -0400

    remove login route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
+  import type { Provider } from '@supabase/supabase-js';
+
+  export let data;
+
+  $: ({ session, supabase } = data);
+
+  const LoginWithProvider = async (provider: Provider) => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider,
+    });
+  };
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
 </script>
 
 <svelte:head>
@@ -6,4 +21,20 @@
   <meta name="description" content="Crew Schedule" />
 </svelte:head>
 
-<section>Home</section>
+{#if !session?.user}
+  <section class="flex flex-col items-center justify-center h-full">
+    <h1 class="h1 -translate-y-10">Sign In With</h1>
+    <div class=" flex gap-4">
+      <button on:click={() => LoginWithProvider('google')}>
+        <i class="fa-brands fa-google fa-2xl"></i>
+      </button>
+      <button on:click={() => LoginWithProvider('github')}>
+        <i class="fa-brands fa-github fa-2xl"></i>
+      </button>
+    </div>
+  </section>
+{:else}
+  <span style="background-image: url('{session.user.user_metadata.avatar_url}')" class="avatar" />
+  <a class="btn variant-filled" href="/map"> Map </a>
+  <button on:click={() => signOut()} class="button">Sign out</button>
+{/if}
