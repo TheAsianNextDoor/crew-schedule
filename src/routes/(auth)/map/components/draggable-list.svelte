365a939d9c06ce048bff2d3d8d5ac7506933f54b
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-10-15 14:54:00 -0400

    add delete button

--- src/routes/(auth)/map/components/draggable-list.svelte
+++ src/routes/(auth)/map/components/draggable-list.svelte
@@ -4,6 +4,8 @@
   import { mapRoutesSubscribe, setMapRoutes } from '../stores/map-routes-store';
   import { onDestroy } from 'svelte';
   import type { HydratedMapMarker } from '../stores/map-marker-store';
+  import { MARKER_PINS, changeMarkerPin } from '../helpers/marker-pin-utils';
+  import { getGoogleMaps } from '$lib/constants/google-maps';
 
   let items: HydratedMapMarker[] = [];
 
@@ -13,38 +15,53 @@
   onDestroy(mapRoutesUnsub);
 
   const flipDurationMs = 100;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  function handleDndConsider<T>(
+
+  const handleDndConsider = <T,>(
     e: CustomEvent<DndEvent<HydratedMapMarker>> & { target: EventTarget & T },
-  ) {
+  ) => {
     const newOrderedList = e.detail.items;
     setMapRoutes(newOrderedList);
-  }
-  function handleDndFinalize<T>(
+  };
+
+  const handleDndFinalize = <T,>(
     e: CustomEvent<DndEvent<HydratedMapMarker>> & { target: EventTarget & T },
-  ) {
+  ) => {
     const newOrderedList = e.detail.items;
     setMapRoutes(newOrderedList);
-  }
+  };
+
+  const deleteItem = (item: HydratedMapMarker) => {
+    const itemIdToRemove = item.id;
+    setMapRoutes(items.filter(({ id }) => id !== itemIdToRemove));
+    changeMarkerPin(item.marker, getGoogleMaps().PinElement, MARKER_PINS.default);
+  };
 </script>
 
-<div class="grid-container w-full">
-  <div>
+<div class="grid-container w-full h-full">
+  <div class="flex flex-col flex-y-1">
     {#each items as _, i}
-      <div class="h-8 m-1">{i}:</div>
+      <div class="flex items-center h-10">
+        <span>{i}:</span>
+      </div>
     {/each}
   </div>
   <section
+    class="flex flex-col gap-y-1 mr-4"
     use:dndzone={{ items, flipDurationMs }}
     on:consider={handleDndConsider}
     on:finalize={handleDndFinalize}
   >
     {#each items as item (item.id)}
       <div
-        class="cursor-grab h-8 border border-black dark:border-slate-400 m-1"
+        class="card cursor-grab border items-center justify-between flex px-2 h-10 border-black bg-slate-300"
         animate:flip={{ duration: flipDurationMs }}
       >
-        {item.site.site_name}
+        <span>
+          {item.site.site_name}
+        </span>
+        <button on:click={() => deleteItem(item)}>
+          <i class="fa-regular fa-circle-xmark"></i>
+        </button>
       </div>
     {/each}
   </section>
