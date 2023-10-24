cce58029e1e494e39d222015164be7e4e8f6e1da
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-10-23 21:57:15 -0400

    add info section

---
+++ src/routes/(auth)/map/components/sidebar/info-section.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let header: string;
+  export let value: Date | string | number | null;
+</script>
+
+<div class="flex gap-4">
+  <h2 class="h5">
+    {header}:
+  </h2>
+  <span>{value}</span>
+</div>
--- src/routes/(auth)/map/components/sidebar/map-search-info.svelte
+++ src/routes/(auth)/map/components/sidebar/map-search-info.svelte
@@ -2,6 +2,7 @@
   import { onDestroy } from 'svelte';
   import { selectedEntitySubscribe } from '../../stores/map-sidebar-store';
   import type { HydratedMapSite } from '../../proxy+page.server';
+  import InfoSection from './info-section.svelte';
 
   let selectedSite: HydratedMapSite | undefined;
 
@@ -12,9 +13,45 @@
 </script>
 
 {#if selectedSite}
-  <div class="h-screen mt-32">
-    <pre>
-      {JSON.stringify(selectedSite, null, 2) || ''}
-    </pre>
+  <div class="h-screen pt-32">
+    <h3 class="h3">Site Info</h3>
+    <div class="px-6">
+      <InfoSection header="Job Number" value={selectedSite.job_number} />
+      <InfoSection header="Site" value={selectedSite.site_name} />
+      <InfoSection header="Client" value={selectedSite.client_name} />
+      <InfoSection header="Estimated Hours" value={selectedSite.estimated_hours} />
+      <InfoSection header="Status" value={selectedSite.status_name} />
+      <InfoSection header="Address" value={selectedSite.address} />
+      <InfoSection header="Scheduled Start Date" value={selectedSite.scheduled_start_date_time} />
+    </div>
+    <div class="py-4">
+      <hr />
+    </div>
+    <h3 class="h3">Phases</h3>
+    {#each selectedSite.phases as phase}
+      <div class="p-4">
+        <div class="border-slate-600 border p-4">
+          <InfoSection header="Foreman" value={phase.foreman_name} />
+          <InfoSection header="Discipline" value={phase.discipline_name} />
+          <InfoSection header="Status" value={phase.status_name} />
+          <InfoSection header="Estimated Hours" value={phase.estimated_hours} />
+          <InfoSection header="Personnel Count" value={phase.personnel_count} />
+          <InfoSection
+            header="Estimated Mobilization"
+            value={phase.estimated_mobilization_duration}
+          />
+          <InfoSection header="Scheduled Start Date" value={phase.scheduled_start_date_time} />
+        </div>
+      </div>
+    {/each}
   </div>
 {/if}
+
+<style>
+  hr {
+    border: none;
+    height: 0.75px;
+    color: black;
+    background-color: black;
+  }
+</style>
