b36139990cffcc886a363488e5c86b0e97530c10
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-10-28 00:28:09 -0400

    add matrix mode button

--- src/routes/(auth)/map/components/button-carousel.svelte
+++ src/routes/(auth)/map/components/button-carousel.svelte
@@ -10,20 +10,11 @@
     getMapMode,
     mapModeStore,
     setMapModeBase,
+    setMapModeMatrix,
     setMapModeRoutes,
   } from '../stores/map-mode-store';
   import { clearMapRoutes } from '../stores/map-routes-store';
   import { clearMapPolylines } from '../stores/map-polyline-store';
-
-  const setRoutesMode = () => {
-    if (getMapMode() === 'routes') {
-      clearMapRoutes();
-      clearMapPolylines();
-      setMapModeBase();
-    } else if (getMapMode() === 'base') {
-      setMapModeRoutes();
-    }
-  };
 
   const handleFilterClick = () => {
     if (isMapFilterVisible()) {
@@ -31,6 +22,24 @@
     } else {
       showMapSidebar();
       showMapFilter();
+    }
+  };
+
+  const handleRoutesClick = () => {
+    if (getMapMode() === 'routes') {
+      clearMapRoutes();
+      clearMapPolylines();
+      setMapModeBase();
+    } else {
+      setMapModeRoutes();
+    }
+  };
+
+  const handleMatrixClick = () => {
+    if (getMapMode() === 'matrix') {
+      setMapModeBase();
+    } else {
+      setMapModeMatrix();
     }
   };
 </script>
@@ -47,9 +56,17 @@
   <button
     class="btn {$mapModeStore === 'routes' &&
       'variant-outline-secondary'} flex justify-center shadow-lg items-center w-20 h-8 rounded-lg bg-surface-100-800-token ml-6"
-    on:click={setRoutesMode}
+    on:click={handleRoutesClick}
   >
     <i class="pr-1 fa-solid fa-route fa-sm"></i>
     Routes
+  </button>
+  <button
+    class="btn {$mapModeStore === 'matrix' &&
+      'variant-outline-secondary'} flex justify-center shadow-lg items-center w-20 h-8 rounded-lg bg-surface-100-800-token ml-6"
+    on:click={handleMatrixClick}
+  >
+    <i class="pr-1 fa-solid fa-bezier-curve fa-sm"></i>
+    Matrix
   </button>
 </div>
--- src/routes/(auth)/map/stores/map-mode-store.ts
+++ src/routes/(auth)/map/stores/map-mode-store.ts
@@ -2,7 +2,7 @@ import { get, writable } from 'svelte/store';
 import { MARKER_PINS, changeMarkerPin } from '../helpers/marker-pin-utils';
 import { getBaseHydratedMarkers } from './map-marker-store';
 
-export type MapMode = 'base' | 'routes';
+export type MapMode = 'base' | 'routes' | 'matrix';
 
 export const mapModeStore = writable<MapMode>('base');
 
@@ -20,4 +20,8 @@ const setMapModeRoutes = () => {
   mapModeStore.set('routes');
 };
 
-export { getMapMode, setMapModeBase, setMapModeRoutes };
+const setMapModeMatrix = () => {
+  mapModeStore.set('matrix');
+};
+
+export { getMapMode, setMapModeBase, setMapModeRoutes, setMapModeMatrix };
