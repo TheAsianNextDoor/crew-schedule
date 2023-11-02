f552f2a2b9ecb64243afcc47cebd44763c7daf58
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-11-01 23:48:39 -0400

    comment out api route

--- src/routes/api/v1/auth/optimal/+server.ts
+++ src/routes/api/v1/auth/optimal/+server.ts
@@ -1,81 +1,81 @@
-import { json, type RequestHandler } from '@sveltejs/kit';
-import { matrixMock } from '../../../../../../mock/matrix';
-import { getGoogleMatrix, type MatrixItem } from '../matrix/get-google-matrix';
-import { optimalMock } from '../../../../../../mock/optimal';
-import { christofidesSerdyukovApproximate } from './christofides';
+// import { json, type RequestHandler } from '@sveltejs/kit';
+// import { matrixMock } from '../../../../../../mock/matrix';
+// import { getGoogleMatrix, type MatrixItem } from '../matrix/get-google-matrix';
+// import { optimalMock } from '../../../../../../mock/optimal';
+// import { christofidesSerdyukovApproximate } from './christofides';
 
-interface PostBody {
-  sites: [string, string][];
-}
+// interface PostBody {
+//   sites: [string, string][];
+// }
 
-const buildAdjacencyMatrix = (matrix: MatrixItem[], matrixWidth: number) => {
-  const adjacencyMatrix: number[][] = [];
-
-  for (let i = 0; i < matrixWidth; i += 1) {
-    const offset = i * matrixWidth;
-    const edges: number[] = [];
-
-    for (let j = 0; j < matrixWidth; j += 1) {
-      const item = matrix[offset + j];
-      if (item.originIndex === item.destinationIndex) {
-        edges.push(0);
-      } else {
-        const numericValue = Number(item.staticDuration?.split('s')[0]);
-        edges.push(numericValue);
-      }
-    }
-
-    adjacencyMatrix[i] = edges;
-  }
-
-  return adjacencyMatrix;
-};
-
-// const buildGraph = (matrix: MatrixItem[], matrixWidth: number) => {
-//   const graph: Record<string, Record<string, number>> = {};
+// const buildAdjacencyMatrix = (matrix: MatrixItem[], matrixWidth: number) => {
+//   const adjacencyMatrix: number[][] = [];
 
 //   for (let i = 0; i < matrixWidth; i += 1) {
 //     const offset = i * matrixWidth;
-//     const node = matrix[offset].originIndex;
-//     const edges: Record<string, number> = {};
+//     const edges: number[] = [];
 
-//     for (let i = 0; i < matrixWidth; i += 1) {
-//       const item = matrix[offset + i];
+//     for (let j = 0; j < matrixWidth; j += 1) {
+//       const item = matrix[offset + j];
 //       if (item.originIndex === item.destinationIndex) {
-//         continue;
+//         edges.push(0);
+//       } else {
+//         const numericValue = Number(item.staticDuration?.split('s')[0]);
+//         edges.push(numericValue);
 //       }
-
-//       const numericValue = Number(item.staticDuration?.split('s')[0]);
-//       edges[item.destinationIndex] = numericValue;
 //     }
 
-//     graph[node] = edges;
+//     adjacencyMatrix[i] = edges;
 //   }
 
-//   return graph;
+//   return adjacencyMatrix;
 // };
 
-export const POST: RequestHandler = async ({ request, fetch }) => {
-  const { sites } = (await request.json()) as PostBody;
-  // const graphWidth = sites.length;
-  const graphWidth = 5;
+// // const buildGraph = (matrix: MatrixItem[], matrixWidth: number) => {
+// //   const graph: Record<string, Record<string, number>> = {};
 
-  // const matrix = await getGoogleMatrix(fetch, sites, sites);
-  const matrix = optimalMock;
-  matrix.sort((a, b) => {
-    if (a.originIndex < b.originIndex || a.destinationIndex < b.destinationIndex) {
-      return -1;
-    }
+// //   for (let i = 0; i < matrixWidth; i += 1) {
+// //     const offset = i * matrixWidth;
+// //     const node = matrix[offset].originIndex;
+// //     const edges: Record<string, number> = {};
 
-    return 1;
-  });
-  const adjacencyMatrix = buildAdjacencyMatrix(matrix, graphWidth);
+// //     for (let i = 0; i < matrixWidth; i += 1) {
+// //       const item = matrix[offset + i];
+// //       if (item.originIndex === item.destinationIndex) {
+// //         continue;
+// //       }
 
-  const parent = christofidesSerdyukovApproximate(adjacencyMatrix);
+// //       const numericValue = Number(item.staticDuration?.split('s')[0]);
+// //       edges[item.destinationIndex] = numericValue;
+// //     }
 
-  // return json({ data });
+// //     graph[node] = edges;
+// //   }
 
-  return json({ data: matrixMock });
+// //   return graph;
+// // };
 
-  // return json({ data: { routes: routesData } });
-};
+// export const POST: RequestHandler = async ({ request, fetch }) => {
+//   const { sites } = (await request.json()) as PostBody;
+//   // const graphWidth = sites.length;
+//   const graphWidth = 5;
+
+//   // const matrix = await getGoogleMatrix(fetch, sites, sites);
+//   const matrix = optimalMock;
+//   matrix.sort((a, b) => {
+//     if (a.originIndex < b.originIndex || a.destinationIndex < b.destinationIndex) {
+//       return -1;
+//     }
+
+//     return 1;
+//   });
+//   const adjacencyMatrix = buildAdjacencyMatrix(matrix, graphWidth);
+
+//   const parent = christofidesSerdyukovApproximate(adjacencyMatrix);
+
+//   // return json({ data });
+
+//   return json({ data: matrixMock });
+
+//   // return json({ data: { routes: routesData } });
+// };
