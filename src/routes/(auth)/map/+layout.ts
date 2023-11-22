0dd51e539debc77a60ea6d8aa7ce1f7bd20cf196
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-11-21 23:59:47 -0500

    prevent multiple google map loads

--- src/routes/(auth)/map/+layout.ts
+++ src/routes/(auth)/map/+layout.ts
@@ -1,5 +1,5 @@
 import { PUBLIC_GOOGLE_MAPS_API_KEY } from '$env/static/public';
-import { setGoogleMaps } from '$lib/constants/google-maps.js';
+import { GOOGLE_MAPS, setGoogleMaps } from '$lib/constants/google-maps.js';
 import { Loader } from '@googlemaps/js-api-loader';
 import { baseHydratedMarkerStore, getSelectedHydratedMarker } from './stores/map-marker-store.js';
 import queryString from 'query-string';
@@ -29,26 +29,28 @@ export type Fields = {
 
 export const ssr = false;
 
-export const load = async ({ data, url, fetch }) => {
-  const loader = new Loader({
-    apiKey: PUBLIC_GOOGLE_MAPS_API_KEY,
-    version: 'beta',
-    libraries: ['core', 'maps', 'marker'],
-  });
-  const { Map, Polyline, InfoWindow } = await loader.importLibrary('maps');
-  const { AdvancedMarkerElement, PinElement } = await loader.importLibrary('marker');
-  const { LatLng } = await loader.importLibrary('core');
-  const { encoding } = await loader.importLibrary('geometry');
+export const load = async ({ data, url }) => {
+  if (!GOOGLE_MAPS) {
+    const loader = new Loader({
+      apiKey: PUBLIC_GOOGLE_MAPS_API_KEY,
+      version: 'beta',
+      libraries: ['core', 'maps', 'marker'],
+    });
+    const { Map, Polyline, InfoWindow } = await loader.importLibrary('maps');
+    const { AdvancedMarkerElement, PinElement } = await loader.importLibrary('marker');
+    const { LatLng } = await loader.importLibrary('core');
+    const { encoding } = await loader.importLibrary('geometry');
 
-  setGoogleMaps({
-    Map,
-    AdvancedMarkerElement,
-    PinElement,
-    LatLng,
-    encoding,
-    Polyline,
-    InfoWindow,
-  });
+    setGoogleMaps({
+      Map,
+      AdvancedMarkerElement,
+      PinElement,
+      LatLng,
+      encoding,
+      Polyline,
+      InfoWindow,
+    });
+  }
 
   const { searchParams } = url;
   const queryParams = queryString.parse(searchParams.toString());
